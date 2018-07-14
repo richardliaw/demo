5140f76fab3da9f8f3221a66e70b0166a4201d3b
Richard Liaw <rliaw@berkeley.edu>
2018-07-14 10:06:51 +0200

    Helper parser

--- keras_mnist_tuned.py
+++ keras_mnist_tuned.py
@@ -86,64 +86,8 @@ def train_mnist(args, cfg, reporter):
 
 
 if __name__ == '__main__':
-    import argparse
-    parser = argparse.ArgumentParser(description='Keras MNIST Example')
-    parser.add_argument(
-        '--jobs',
-        type=int,
-        default=1,
-        help='number of jobs to run concurrently (default: 1)')
-    parser.add_argument(
-        '--threads',
-        type=int,
-        default=None,
-        help='threads used in operations (default: all)')
-    parser.add_argument(
-        '--steps',
-        type=float,
-        default=0.01,
-        metavar='LR',
-        help='learning rate (default: 0.01)')
-    parser.add_argument(
-        '--lr',
-        type=float,
-        default=0.01,
-        metavar='LR',
-        help='learning rate (default: 0.01)')
-    parser.add_argument(
-        '--momentum',
-        type=float,
-        default=0.5,
-        metavar='M',
-        help='SGD momentum (default: 0.5)')
-    parser.add_argument(
-        '--kernel1',
-        type=int,
-        default=3,
-        help='Size of first kernel (default: 3)')
-    parser.add_argument(
-        '--kernel2',
-        type=int,
-        default=3,
-        help='Size of second kernel (default: 3)')
-    parser.add_argument(
-        '--poolsize', type=int, default=2, help='Size of Pooling (default: 2)')
-    parser.add_argument(
-        '--dropout1',
-        type=float,
-        default=0.25,
-        help='Size of first kernel (default: 0.25)')
-    parser.add_argument(
-        '--hidden',
-        type=int,
-        default=128,
-        help='Size of Hidden Layer (default: 128)')
-    parser.add_argument(
-        '--dropout2',
-        type=float,
-        default=0.5,
-        help='Size of first kernel (default: 0.5)')
-
+    from helper import create_parser
+    parser = create_parser()
     args = parser.parse_args()
     mnist.load_data()  # we do this because it's not threadsafe
 
